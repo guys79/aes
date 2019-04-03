600a2a9e97b11e7c961f5788ddee07f1dabc29ae
guys79 <guys797@gmail.com>
2019-04-03 11:59:46 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * This class will be the class that gets the command line and activates the correct command
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
